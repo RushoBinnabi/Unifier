e55c87508ce20d9e6d65c3f693ab9a2cf97d41a8
RushoBinnabi <RushoBinnabi123@yahoo.com>
2024-01-13 17:19:04 -0500

    added more encapsulation to the functions of the app

--- app/src/main/java/rusho/app/unifer/NumberGenerator.java
+++ app/src/main/java/rusho/app/unifer/NumberGenerator.java
@@ -2,8 +2,6 @@
  * Name: Rusho Binnabi
  * Date: 7/21/2023
  * Project: Unifier App - Number Generator Function
- * Contact Information: RushoBinnabi123@yahoo.com
- * Code File Updated: 7/21/2023 at 11:23 PM
  */
 
 package rusho.app.unifer;
@@ -28,17 +26,190 @@ public class NumberGenerator extends AppCompatActivity {
 
     // this class generates a random number between a range of two numbers entered by the user.
 
-    Button clearNumberButton;
-    Button generateNumberButton;
-    String number1text;
-    String number2text;
-    int number1;
-    int number2;
-    int randomNumber;
-    EditText enterNumber1;
-    EditText enterNumber2;
-    Random numberGenerator;
+    private Button clearNumberButton;
+    private Button generateNumberButton;
+    private String number1Text;
+    private String number2Text;
+    private int number1;
+    private int number2;
+    private int randomNumber;
+    private EditText enterNumber1;
+    private EditText enterNumber2;
+    private Random numberGenerator;
 
+    /**
+     * this setClearNumberButton() method sets the clear number button.
+     */
+    public void setClearNumberButton() {
+        clearNumberButton = findViewById(R.id.ClearNumberButton);
+    }
+
+    /**
+     * this getClearNumberButton() method gets the clear number button.
+     * @return the clear number button.
+     */
+
+    public Button getClearNumberButton() {
+        return clearNumberButton;
+    }
+
+    /**
+     * this setGenerateNumberButton() method sets the generate number button.
+     */
+
+    public void setGenerateNumberButton() {
+        generateNumberButton = findViewById(R.id.GenerateNumberButton);
+    }
+
+    /**
+     * this setRandomNumber() method sets the random number.
+     * @param number the random number being set.
+     */
+
+    public void setRandomNumber(int number) {
+        randomNumber = number;
+    }
+
+    /**
+     * this getRandomNumber() method gets the random number.
+     * @return the random number.
+     */
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    /**
+     * this setNumber1Text() method sets the text for the 1st number.
+     * @param string the text for the 1st number.
+     */
+
+    public void setNumber1Text(String string) {
+        number1Text = string;
+    }
+
+    /**
+     * this getNumber1Text() method gets the text for the 1st number.
+     * @return the text for the 1st number.
+     */
+
+    public String getNumber1Text() {
+        return number1Text;
+    }
+
+    /**
+     * this setNumber2Text() method sets the text for the 2nd number.
+     * @param string the text for the 1st number.
+     */
+
+    public void setNumber2Text(String string) {
+        number2Text = string;
+    }
+
+    /**
+     * this getNumber2Text() method gets the text for the 2nd number.
+     * @return the text for the 2nd number.
+     */
+
+    public String getNumber2Text() {
+        return number2Text;
+    }
+
+    /**
+     * this setNumber1() method sets the 1st number.
+     * @param number the 1st number.
+     */
+
+    public void setNumber1(int number) {
+        number1 = number;
+    }
+
+    /**
+     * this getNumber1() method gets the 1st number.
+     * @return the 1st number.
+     */
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    /**
+     * this setNumber2() method sets the 2nd number.
+     * @param number the 2nd number.
+     */
+
+    public void setNumber2(int number) {
+        number2 = number;
+    }
+
+    /**
+     * this getNumber2() method gets the 2nd number.
+     * @return the 2nd number.
+     */
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    /**
+     * this getEnterNumber1() method gets the edit text for the 1st number.
+     * @return the edit text for the 1st number.
+     */
+
+    public EditText getEnterNumber1() {
+        return enterNumber1;
+    }
+
+    /**
+     * this setEnterNumber1() method sets the edit text for the 1st number.
+     */
+
+    public void setEnterNumber1() {
+        enterNumber1 = findViewById(R.id.EnterNumber1);
+    }
+
+    /**
+     * this getEnterNumber2() method gets the edit text for the 2nd number.
+     * @return the edit text for the 2nd number.
+     */
+
+    public EditText getEnterNumber2() {
+        return enterNumber2;
+    }
+
+    /**
+     * this setEnterNumber2() method sets the edit text for the 2nd number.
+     */
+
+    public void setEnterNumber2() {
+        enterNumber2 = findViewById(R.id.EnterNumber2);
+    }
+
+    /**
+     * this setNumberGenerator() method sets the number generator.
+     */
+
+    public void setNumberGenerator() {
+        numberGenerator = new Random();
+    }
+
+    /**
+     * this getNumberGenerator() method gets the number generator.
+     * @return the number generator.
+     */
+
+    public Random getNumberGenerator() {
+        return numberGenerator;
+    }
+
+    /**
+     * this getGenerateNumberButton() method gets the generate number button.
+     * @return the generate number button.
+     */
+
+    public Button getGenerateNumberButton() {
+        return generateNumberButton;
+    }
     /**
      * this protected onCreate() method, which uses the Bundle savedInstanceObject as its argument, 
      * has the code for the Number Generator function that the Unifier app needs to function.
@@ -50,8 +221,8 @@ public class NumberGenerator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_generator);
         activate();
-        clearNumberButton.setOnClickListener(v -> clearNumber());
-        generateNumberButton.setOnClickListener(v -> generateNumber());
+        getClearNumberButton().setOnClickListener(v -> clearNumber());
+        getGenerateNumberButton().setOnClickListener(v -> generateNumber());
     }
 
     /**
@@ -70,11 +241,11 @@ public class NumberGenerator extends AppCompatActivity {
 
     private void generateNumber() {
         try {
-            number1text = enterNumber1.getText().toString();
-            number2text = enterNumber2.getText().toString();
-            number1 = Integer.parseInt(number1text);
-            number2 = Integer.parseInt(number2text);
-            randomNumber = numberGenerator.nextInt((number2 - number1) + 1) + number1; // randomNumber will have the number that is generated by the range of two numbers within number1 and number2.
+            setNumber1Text(getEnterNumber1().getText().toString());
+            setNumber2Text(getEnterNumber2().getText().toString());
+            setNumber1(Integer.parseInt(getNumber1Text()));
+            setNumber2(Integer.parseInt(getNumber2Text()));
+            setRandomNumber(getNumberGenerator().nextInt((getNumber2() - getNumber1()) + 1) + getNumber1()); // randomNumber will have the number that is generated by the range of two numbers within number1 and number2.
             showRandomNumber();
         }
         catch (NumberFormatException n) {
@@ -89,7 +260,7 @@ public class NumberGenerator extends AppCompatActivity {
      */
 
     private void showRandomNumber() {
-        Toast toast = Toast.makeText(this, "Randomly generated number between " + number1 + " and " + number2 + ": " + randomNumber, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, "Randomly generated number between " + getNumber1() + " and " + getNumber2() + ": " + getRandomNumber(), Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.BOTTOM, 0, 120);
         toast.show();
     }
@@ -99,13 +270,13 @@ public class NumberGenerator extends AppCompatActivity {
      */
 
     private void clearNumber() {
-        number1text = "";
-        number2text = "";
-        number1 = 0;
-        number2 = 0;
-        randomNumber = 0;
-        enterNumber1.getText().clear();
-        enterNumber2.getText().clear();
+        setNumber1Text("");
+        setNumber2Text("");
+        setNumber1(0);
+        setNumber2(0);
+        setRandomNumber(0);
+        getEnterNumber1().getText().clear();
+        getEnterNumber2().getText().clear();
     }
 
     /**
@@ -113,16 +284,16 @@ public class NumberGenerator extends AppCompatActivity {
      */
 
     private void activate() {
-        clearNumberButton = findViewById(R.id.ClearNumberButton);
-        generateNumberButton = findViewById(R.id.GenerateNumberButton);
-        numberGenerator = new Random();
-        number1text = "";
-        number2text = "";
-        number1 = 0;
-        number2 = 0;
-        randomNumber = 0;
-        enterNumber1 = findViewById(R.id.EnterNumber1);
-        enterNumber2 = findViewById(R.id.EnterNumber2);
+        setClearNumberButton();
+        setGenerateNumberButton();
+        setNumberGenerator();
+        setNumber1Text("");
+        setNumber2Text("");
+        setNumber1(0);
+        setNumber2(0);
+        setRandomNumber(0);
+        setEnterNumber1();
+        setEnterNumber2();
     }
 
 }
--- app/src/main/java/rusho/app/unifer/PasswordGenerator.java
+++ app/src/main/java/rusho/app/unifer/PasswordGenerator.java
@@ -2,8 +2,6 @@
  * Name: Rusho Binnabi
  * Date: 1/2/2023
  * Project: Unifier App - Password Generator Function
- * Code File Updated: 4/16/2023 at 11:03 PM
- * Contact Information: RushoBinnabi123@yahoo.com 
  */
 
 package rusho.app.unifer;
@@ -28,16 +26,198 @@ public class PasswordGenerator extends AppCompatActivity {
 
     // this class generates random passwords based on a length specified by the user.
 
-    EditText userInput;
-    TextView passwordOutput;
-    Button passwordGeneratorResetButton; 
-    Button passwordGeneratorGeneratePasswordButton;
-    String generatedPassword;
-    int number;
-    String lowercaseLetters;
-    String uppercaseLetters;
-    String numbers;
-    String symbols;
+    private EditText userInput;
+    private TextView passwordOutput;
+    private Button passwordGeneratorResetButton;
+    private Button passwordGeneratorGeneratePasswordButton;
+    private String generatedPassword;
+    private int number;
+    private String lowercaseLetters;
+    private String uppercaseLetters;
+    private String numbers;
+    private String symbols;
+
+    /**
+     * this getUserInput() method gets the edit text for the user input.
+     * @return the edit text for the user input.
+     */
+
+    public EditText getUserInput() {
+        return userInput;
+    }
+
+    /**
+     * this setUserInput() method sets the edit text for the user input.
+     */
+
+    public void setUserInput() {
+        userInput = findViewById(R.id.PasswordGeneratorLengthInput);
+    }
+
+    /**
+     * this getPasswordOutput() method gets the text view for the password output.
+     * @return the text view for the password output.
+     */
+
+    public TextView getPasswordOutput() {
+        return passwordOutput;
+    }
+
+    /**
+     * this setPasswordOutput() method sets the text view for the password output.
+     */
+
+    public void setPasswordOutput() {
+        passwordOutput = findViewById(R.id.PasswordGeneratorGeneratedPasswordOutput);
+    }
+
+    /**
+     * this getPasswordGeneratorResetButton() method gets the reset button.
+     * @return the reset button.
+     */
+
+    public Button getPasswordGeneratorResetButton() {
+        return passwordGeneratorResetButton;
+    }
+
+    /**
+     * this setPasswordGeneratorResetButton() method sets the reset button.
+     */
+
+    public void setPasswordGeneratorResetButton() {
+        passwordGeneratorResetButton = findViewById(R.id.PasswordGeneratorResetButton);
+    }
+
+    /**
+     * this setPasswordGeneratorGeneratePasswordButton() method sets the generate password button.
+     */
+
+    public void setPasswordGeneratorGeneratePasswordButton() {
+        passwordGeneratorGeneratePasswordButton = findViewById(R.id.PasswordGeneratorGeneratePasswordsButton);
+    }
+
+    /**
+     * this getPasswordGeneratorGeneratePasswordButton() method gets the generate password button.
+     * @return the generate password button.
+     */
+    public Button getPasswordGeneratorGeneratePasswordButton() {
+        return passwordGeneratorGeneratePasswordButton;
+    }
+
+    /**
+     * this getGeneratedPassword() method gets the generated password.
+     * @return the generated password.
+     */
+
+    public String getGeneratedPassword() {
+        return generatedPassword;
+    }
+
+    /**
+     * this setGeneratedPassword() method sets the generated password.
+     * @param string the generated password being set
+     */
+
+    public void setGeneratedPassword(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == ' ') { // if the text is empty,
+                generatedPassword = string; // then the generated password is cleared.
+            }
+            else {
+                generatedPassword += string; // otherwise, the generated password gets added onto the end.
+            }
+        }
+    }
+
+    /**
+     * this getNumber() method gets the number.
+     * @return the number.
+     */
+
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * this setNumber() method sets the number.
+     * @param number the number being set.
+     */
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * this setUppercaseLetters() method sets the uppercase letters.
+     * @param string the uppercase letters being set.
+     */
+
+    public void setUppercaseLetters(String string) {
+        uppercaseLetters = string;
+    }
+
+    /**
+     * this getUppercaseLetters() method gets the uppercase letters.
+     * @return the uppercase letters.
+     */
+
+    public String getUppercaseLetters() {
+        return uppercaseLetters;
+    }
+
+    /**
+     * this setLowercaseLetters() method sets the lowercase letters.
+     * @param string the lowercase letters being set.
+     */
+
+    public void setLowercaseLetters(String string) {
+        lowercaseLetters = string;
+    }
+
+    /**
+     * this getLowercaseLetters() method gets the lowercase letters.
+     * @return the lowercase letters.
+     */
+
+    public String getLowercaseLetters() {
+        return lowercaseLetters;
+    }
+
+    /**
+     * this setNumbers() method sets the numbers.
+     * @param string the numbers being set.
+     */
+
+    public void setNumbers(String string) {
+        numbers = string;
+    }
+
+    /**
+     * this getNumbers() method gets the numbers.
+     * @return the numbers.
+     */
+
+    public String getNumbers() {
+        return numbers;
+    }
+
+    /**
+     * this setSymbols() method sets the symbols.
+     * @param string the symbols being set.
+     */
+
+    public void setSymbols(String string) {
+        symbols = string;
+    }
+
+    /**
+     * this getSymbols() method gets the symbols.
+     * @return the symbols.
+     */
+
+    public String getSymbols() {
+        return symbols;
+    }
 
     /*
     * this protected onCreate() method, which uses the Bundle savedInstanceObject as its argument, has the code for the Password Generator function that the Unifier app needs to function.
@@ -48,8 +228,8 @@ public class PasswordGenerator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_generator);
         activate();
-        passwordGeneratorResetButton.setOnClickListener(v -> resetValues());
-        passwordGeneratorGeneratePasswordButton.setOnClickListener(v -> generatePasswords());
+        getPasswordGeneratorResetButton().setOnClickListener(v -> resetValues());
+        getPasswordGeneratorGeneratePasswordButton().setOnClickListener(v -> generatePasswords());
     }
 
     /**
@@ -57,14 +237,14 @@ public class PasswordGenerator extends AppCompatActivity {
      */
 
     private void resetValues() {
-        userInput.getText().clear();
-        passwordOutput.setText("");
-        generatedPassword = "";
-        lowercaseLetters = "";
-        uppercaseLetters = "";
-        numbers = "";
-        symbols = "";
-        number = 0;
+        getUserInput().getText().clear();
+        getPasswordOutput().setText("");
+        setGeneratedPassword("");
+        setLowercaseLetters("");
+        setUppercaseLetters("");
+        setNumbers("");
+        setSymbols("");
+        setNumber(0);
         reActivate();
     }
 
@@ -73,16 +253,16 @@ public class PasswordGenerator extends AppCompatActivity {
      */
 
     private void activate() {
-        userInput = findViewById(R.id.PasswordGeneratorLengthInput);
-        passwordOutput = findViewById(R.id.PasswordGeneratorGeneratedPasswordOutput);
-        passwordGeneratorResetButton = findViewById(R.id.PasswordGeneratorResetButton);
-        passwordGeneratorGeneratePasswordButton = findViewById(R.id.PasswordGeneratorGeneratePasswordsButton);
-        number = 0;
-        generatedPassword = "";
-        lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-        uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        numbers = "0123456789";
-        symbols = "!@#$%^&*()_-+={[}]:;?/>.<,/*-+'";
+        setUserInput();
+        setPasswordOutput();
+        setPasswordGeneratorResetButton();
+        setPasswordGeneratorGeneratePasswordButton();
+        setNumber(0);
+        setGeneratedPassword("");
+        setLowercaseLetters("abcdefghijklmnopqrstuvwxyz");
+        setUppercaseLetters("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        setNumbers("0123456789");
+        setSymbols("!@#$%^&*()_-+={[}]:;?/>.<,/*-+'");
     }
 
     /**
@@ -91,10 +271,10 @@ public class PasswordGenerator extends AppCompatActivity {
      */
 
     private void reActivate() {
-        lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-        uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        numbers = "0123456789";
-        symbols = "!@#$%^&*()_-+={[}]:;?/>.<,/*-+'";
+        setLowercaseLetters("abcdefghijklmnopqrstuvwxyz");
+        setUppercaseLetters("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        setNumbers("0123456789");
+        setSymbols("!@#$%^&*()_-+={[}]:;?/>.<,/*-+'");
     }
 
 
@@ -106,34 +286,34 @@ public class PasswordGenerator extends AppCompatActivity {
     
     private void generatePasswords() {
         try {
-            generatedPassword = userInput.getText().toString();
-            number = Integer.parseInt(generatedPassword);
-            generatedPassword = ""; // generatedPassword gets set to an empty string in order to avoid any errors or undesired lengths in the generated password.
-            for (int i = 0; i < number; i++) {
+            setGeneratedPassword(getUserInput().getText().toString());
+            setNumber(Integer.parseInt(getGeneratedPassword()));
+            setGeneratedPassword(""); // generatedPassword gets set to an empty string in order to avoid any errors or undesired lengths in the generated password.
+            for (int i = 0; i < getNumber(); i++) {
                 int random = 1 + (int) (5 * Math.random());
                 switch (random) {
                     case 1:
-                        generatedPassword += String.valueOf((int)(0 * Math.random()));
+                        setGeneratedPassword(String.valueOf((int)(0 * Math.random())));
                         break;
                     case 2:
-                        random = (int) (lowercaseLetters.length() * Math.random());
-                        generatedPassword += String.valueOf(lowercaseLetters.charAt(random));
+                        random = (int) (getLowercaseLetters().length() * Math.random());
+                        setGeneratedPassword(String.valueOf(getLowercaseLetters().charAt(random)));
                         break;
                     case 3:
-                        random = (int) (uppercaseLetters.length() * Math.random());
-                        generatedPassword += String.valueOf(uppercaseLetters.charAt(random));
+                        random = (int) (getUppercaseLetters().length() * Math.random());
+                        setGeneratedPassword(String.valueOf(getUppercaseLetters().charAt(random)));
                         break;
                     case 4:
-                        random = (int) (numbers.length() * Math.random());
-                        generatedPassword += String.valueOf(numbers.charAt(random));
+                        random = (int) (getNumbers().length() * Math.random());
+                        setGeneratedPassword(String.valueOf(getNumbers().charAt(random)));
                         break;
                     case 5:
-                        random = (int) (symbols.length() * Math.random());
-                        generatedPassword += String.valueOf(symbols.charAt(random));
+                        random = (int) (getSymbols().length() * Math.random());
+                        setGeneratedPassword(String.valueOf(getSymbols().charAt(random)));
                         break;
                 }
             }
-            passwordOutput.setText(String.format("Generated Password of Length %d is: \n\n %s", number, generatedPassword));
+            getPasswordOutput().setText(String.format("Generated Password of Length %d is: \n\n %s", getNumber(), getGeneratedPassword()));
         }
         catch (NumberFormatException n)  {
             Toast toast = Toast.makeText(this, "Error. Please enter a number", Toast.LENGTH_SHORT); 
